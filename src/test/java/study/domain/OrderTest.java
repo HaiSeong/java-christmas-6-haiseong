6d64f66d79c30dc0e7f416f5b2d10b0b19a87aba
haiseong <wjdgotjd9908@gmail.com>
2023-11-12 02:41:34 +0900

    refactor(OrderTest): beforeEach로 중복 코드 제거

--- src/test/java/study/domain/OrderTest.java
+++ src/test/java/study/domain/OrderTest.java
@@ -7,15 +7,22 @@ import christmas.enums.MenuItem;
 import christmas.domain.Order;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
 
+    private Map<MenuItem, Integer> items;
+
+    @BeforeEach
+    void setUp() {
+        items = new HashMap<>();
+    }
+
     @Test
     @DisplayName("정상적인 주문이 생성되는지 확인")
     void Order_생성_정상적인_경우() {
-        Map<MenuItem, Integer> items = new HashMap<>();
         items.put(MenuItem.MUSHROOM_SOUP, 1);
         items.put(MenuItem.T_BONE_STEAK, 1);
 
@@ -27,15 +34,12 @@ public class OrderTest {
     @Test
     @DisplayName("음료만 주문할 경우 예외 발생을 확인")
     void Order_빈_주문() {
-        Map<MenuItem, Integer> items = new HashMap<>();
-
         assertThatThrownBy(() -> new Order(items)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("음료만 주문할 경우 예외 발생을 확인")
     void Order_생성_음료만_주문() {
-        Map<MenuItem, Integer> items = new HashMap<>();
         items.put(MenuItem.ZERO_COLA, 3);
 
         assertThatThrownBy(() -> new Order(items)).isInstanceOf(IllegalArgumentException.class)
@@ -45,7 +49,6 @@ public class OrderTest {
     @Test
     @DisplayName("주문 항목의 최대 개수를 초과할 경우 예외 발생을 확인")
     void Order_생성_너무_많은_양의_주문_항목() {
-        Map<MenuItem, Integer> items = new HashMap<>();
         items.put(MenuItem.T_BONE_STEAK, 21);
 
         assertThatThrownBy(() -> new Order(items)).isInstanceOf(IllegalArgumentException.class)
