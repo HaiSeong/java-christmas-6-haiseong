c4f861013bed217a7c810852a3458a58ab39737b
haiseong <wjdgotjd9908@gmail.com>
2023-11-12 03:11:36 +0900

    feat(SpecialDiscountPolicy, SpecialDiscountPolicyTest): 특별 할인 정책 기능 구현

---
+++ src/main/java/christmas/domain/discount/SpecialDiscountPolicy.java
@@ -0,0 +1,23 @@
+package christmas.domain.discount;
+
+import christmas.domain.Date;
+import christmas.domain.Order;
+
+public class SpecialDiscountPolicy implements DiscountPolicy {
+    private static final String POLICY_NAME = "특별 할인";
+    private static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
+
+    private final Date date;
+
+    public SpecialDiscountPolicy(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public DiscountEntry applyDiscount(Order order) {
+        if (date.isSpecialDay()) {
+            return new DiscountEntry(POLICY_NAME, SPECIAL_DISCOUNT_AMOUNT);
+        }
+        return new DiscountEntry(POLICY_NAME, 0);
+    }
+}
---
+++ src/test/java/study/domain/discount/SpecialDiscountPolicyTest.java
@@ -0,0 +1,47 @@
+package study.domain.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.Date;
+import christmas.domain.Order;
+import christmas.domain.discount.DiscountEntry;
+import christmas.domain.discount.DiscountPolicy;
+import christmas.domain.discount.SpecialDiscountPolicy;
+import christmas.domain.discount.WeekendDiscountPolicy;
+import christmas.enums.MenuItem;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SpecialDiscountPolicyTest {
+
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order(Map.of(MenuItem.T_BONE_STEAK, 1, MenuItem.CHOCOLATE_CAKE, 1, MenuItem.ICE_CREAM, 2));
+    }
+
+    @Test
+    @DisplayName("스페셜 데이에 할인이 올바르게 적용되는지 테스트")
+    void applyDiscountBeforeChristmas() {
+        Date specialDay = new Date(25);
+        DiscountPolicy policy = new SpecialDiscountPolicy(specialDay);
+
+        DiscountEntry discount = policy.applyDiscount(order);
+
+        assertThat(discount.getValue()).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("스페셜 데이가 아닌 날에 할인이 적용되지 않는지 테스트")
+    void applyDiscountAfterChristmas() {
+        Date ordinalDay = new Date(26);
+        DiscountPolicy policy = new SpecialDiscountPolicy(ordinalDay);
+
+        DiscountEntry discount = policy.applyDiscount(order);
+
+        assertThat(discount.getValue()).isEqualTo(0);
+    }
+}
