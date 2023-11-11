fd8433d7e7949b6d8bba4fd51767ff88d44eefda
haiseong <wjdgotjd9908@gmail.com>
2023-11-12 01:09:28 +0900

    feat(Date, DateTest): 날짜 객체 생성, 유효성 검사

---
+++ src/main/java/christmas/domain/Date.java
@@ -0,0 +1,41 @@
+package christmas.domain;
+
+import java.util.Set;
+
+public class Date {
+    private static final int MINIMUM_DAY = 1;
+    private static final int MAXIMUM_DAY = 31;
+    private static final int CHRISTMAS_DAY = 25;
+    private static final Set<Integer> WEEKEND_DAYS = Set.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private static final Set<Integer> SPECIAL_DAYS = Set.of(3, 10, 17, 24, 25, 31);
+    private static final String INVALID_DAY_RANGE_ERROR_MESSAGE = "날짜는 1일에서 31일 사이여야 합니다.";
+
+    private final int day;
+
+    public Date(int day) {
+        validate(day);
+        this.day = day;
+    }
+
+    private void validate(int day) {
+        if (day < MINIMUM_DAY || day > MAXIMUM_DAY) {
+            throw new IllegalArgumentException(INVALID_DAY_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public boolean isBeforeChristmas() {
+        return day <= CHRISTMAS_DAY;
+    }
+
+    public boolean isWeekend() {
+        return WEEKEND_DAYS.contains(day);
+    }
+
+    public boolean isSpecialDay() {
+        return SPECIAL_DAYS.contains(day);
+    }
+}
---
+++ src/test/java/study/domain/DateTest.java
@@ -0,0 +1,75 @@
+package study.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import christmas.domain.Date;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+    @Test
+    @DisplayName("유효한 날짜가 올바르게 처리되는지 확인")
+    void validDayTest() {
+        Date date = new Date(15);
+        assertThat(date.getDay()).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 날짜가 예외를 발생시키는지 확인")
+    void invalidDayTest() {
+        assertThatThrownBy(() -> new Date(32))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("날짜는 1일에서 31일 사이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("주말 여부가 올바르게 확인되는지 테스트")
+    void isWeekendTest() {
+        Date weekendDate = new Date(1);
+
+        assertThat(weekendDate.isWeekend()).isTrue();
+    }
+
+    @Test
+    @DisplayName("주말이 아닌 날짜가 올바르게 처리되는지 확인")
+    void isNotWeekendTest() {
+        Date notWeekendDate = new Date(3);
+
+        assertThat(notWeekendDate.isWeekend()).isFalse();
+    }
+
+    @Test
+    @DisplayName("특별한 날 여부가 올바르게 확인되는지 테스트")
+    void isSpecialDayTest() {
+        Date specialDate = new Date(25);
+
+        assertThat(specialDate.isSpecialDay()).isTrue();
+    }
+
+    @Test
+    @DisplayName("특별하지 않은 날짜가 올바르게 처리되는지 확인")
+    void isNotSpecialDayTest() {
+        Date notSpecialDate = new Date(4);
+
+        assertThat(notSpecialDate.isSpecialDay()).isFalse();
+    }
+
+    @Test
+    @DisplayName("크리스마스 이전 날짜가 올바르게 처리되는지 확인")
+    void isChristmasEveDiscountTest() {
+        Date notSpecialDate = new Date(25);
+
+        assertThat(notSpecialDate.isBeforeChristmas()).isTrue();
+    }
+
+    @Test
+    @DisplayName("크리스마스 이후 날짜가 올바르게 처리되는지 확인")
+    void isNotChristmasEveDiscountTest() {
+        Date notSpecialDate = new Date(26);
+
+        assertThat(notSpecialDate.isBeforeChristmas()).isFalse();
+    }
+}
+
