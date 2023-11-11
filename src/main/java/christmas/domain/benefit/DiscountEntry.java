224e575c49a350cee8171380ffa4fdf8f63b08ad
haiseong <wjdgotjd9908@gmail.com>
2023-11-12 05:28:01 +0900

    fix(DiscountEntry): 오타 수정

--- src/main/java/christmas/domain/benefit/DiscountEntry.java
+++ src/main/java/christmas/domain/benefit/DiscountEntry.java
@@ -3,17 +3,17 @@ package christmas.domain.benefit;
 import java.util.Map.Entry;
 
 public class DiscountEntry implements Entry<String, Integer> {
-    private final String polocyName;
+    private final String policyName;
     private Integer discountAmount;
 
-    public DiscountEntry(String polocyName, Integer discountAmount) {
-        this.polocyName = polocyName;
+    public DiscountEntry(String policyName, Integer discountAmount) {
+        this.policyName = policyName;
         this.discountAmount = discountAmount;
     }
 
     @Override
     public String getKey() {
-        return polocyName;
+        return policyName;
     }
 
     @Override
